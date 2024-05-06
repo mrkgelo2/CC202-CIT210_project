d7c002d20e0f20206d930c66b6b92c2c5dddca4f
Mark Angelo Servando <143057502+mrkgelo2@users.noreply.github.com>
2024-05-07 00:47:18 +0800

    Add files via upload

---
+++ src/admin/admin_partials/_sidebar.php
@@ -0,0 +1,56 @@
+
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+  <ul class="nav">
+    <li class="nav-item">
+      <a class="nav-link" href="./dashboard.php">
+        <i class="icon-grid menu-icon"></i>
+        <span class="menu-title">Dashboard</span>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="./announcement.php">
+        <i class="mdi mdi-bullhorn menu-icon"></i>
+        <span class="menu-title">Announcements</span>
+      </a>
+
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="./officials.php">
+        <i class="mdi mdi-account-multiple-outline menu-icon"></i>
+        <span class="menu-title">Officials/Staff</span>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="./residents.php">
+        <i class="mdi mdi-human-male-female menu-icon"></i>
+        <span class="menu-title">Residents Records</span>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+        <i class="mdi mdi-file menu-icon"></i>
+        <span class="menu-title">Certificate Requests</span>
+        <i class="menu-arrow"></i>
+      </a>
+      <div class="collapse" id="ui-basic">
+        <ul class="nav flex-column sub-menu">
+          <li class="nav-item"> <a class="nav-link" href="./brgyclearance.php">Brgy Clearance</a></li>
+          <li class="nav-item"> <a class="nav-link" href="./certofindigency.php">Certificate of Indigency</a></li>
+          <li class="nav-item"> <a class="nav-link" href="./businesspermit.php">Brgy Business Permit</a></li>
+        </ul>
+      </div>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="./blotterrep.php">
+        <i class="mdi mdi-alert-box menu-icon"></i>
+        <span class="menu-title">Blotter Reports</span>
+      </a>
+    </li>
+   <!-- <li class="nav-item">
+      <a class="nav-link" data-bs-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
+        <i class="mdi mdi-home menu-icon"></i>
+        <span class="menu-title">Household Records</span>
+      </a>
+    </li> -->
+  </ul>
+</nav>
