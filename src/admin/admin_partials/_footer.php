a441466ac96bb3ab2c3e79275e6d5d4e86743797
Mark Angelo Servando <143057502+mrkgelo2@users.noreply.github.com>
2024-05-09 15:57:00 +0800

    Delete src/admin directory

--- src/admin/admin_partials/_footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="footer">
-  <div class="d-sm-flex justify-content-center justify-content-sm-between">
-    <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2024. Final Project for CC205 and CIT210. All rights reserved.</span>
-    <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made by Group ni Kenneth.</span>
-  </div>
-</footer>
